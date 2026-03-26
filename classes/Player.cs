e66e2f10d3b6abbb2755847032911eb123d55e13
MATFIX-coder <aksdim82@gmail.com>
2026-03-27 00:09:42 +0500

    доделал

---
+++ classes/Player.cs
@@ -0,0 +1,36 @@
+namespace ShahtyorGame.classes
+{
+    public class Player
+    {
+        public int X {get; set; } //позиция по иксу
+        public int Y {get; set; } //позиция по игрику
+        public int Health {get; set; } //здоровье
+        public int PickaxeStrength {get; set; } //прочность кирки
+        public int Coins {get; set; } //кол-во монет
+
+        public Player() //конструктор для игрока
+        {
+            X = 0;
+            Y = 0;
+            Health = 100;
+            PickaxeStrength = 100;
+            Coins = 0;
+        }
+
+        public void ResetPosition() //сброс позиции после завершения уровня
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public bool IsAlive() //проверка, что жив
+        {
+            return Health > 0;
+        }
+
+        public bool BrokePickaxe() //проверка целости кирки
+        {
+            return PickaxeStrength > 0;
+        }
+    }
+}
