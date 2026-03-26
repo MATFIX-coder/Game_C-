ada471da352ef9204897809997e0e2b24c3cf661
MATFIX-coder <aksdim82@gmail.com>
2026-03-27 00:08:27 +0500

    Создал базовые классы

---
+++ Forms/Form1.cs
@@ -0,0 +1,9 @@
+namespace ShahtyorGame;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ enums/CellType.cs
@@ -0,0 +1,11 @@
+namespace ShahtyorGame.enums
+{
+    public enum CellType //создал enum колликция элементов на поле
+    {
+        EmptyPoint,      // Пустая клетка
+        Artifact,   // Артефакт
+        Mine,       // Мина 
+        Pit,      // Пропасть
+        Stairs    //Лестницы  
+    }
+}
